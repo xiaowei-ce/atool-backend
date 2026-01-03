41e9a8c5d8f83d4d43740699aa11d48c17c7ca65
xiaowei-ce <starryflow@outlook.com>
2026-01-03 14:14:38 +0800

    commit

--- src/main/java/org/example/atool/AtoolApplication.java
+++ src/main/java/org/example/atool/AtoolApplication.java
@@ -1,11 +1,13 @@
 package org.example.atool;
 
 import com.dtflys.forest.springboot.annotation.ForestScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @ForestScan(basePackages = "org.example.atool.HttpApi")
+@MapperScan(basePackages = "org.example.atool.mapper")
 public class AtoolApplication {
 
     public static void main(String[] args) {
